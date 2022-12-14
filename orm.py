aed5d7fd0c94f1fb915ce944e6f1e79fd69cf01d
Nikita Miller <milleriscream@gmail.com>
2022-12-14 16:22:00 +0200

    crud

--- orm.py
+++ orm.py
@@ -2,10 +2,11 @@ import mysql.connector
 import random
 import hashlib
 import uuid
+import time
 
 class User:
-    def __init__(self,  cursor = None):
-        if cursor == None:
+    def __init__(self,  row = None):
+        if row == None:
             self.id         = None
             self.username   = None
             self.password   = None
@@ -14,18 +15,22 @@ class User:
             self.avatar     = None
             self.email      = None
             self.email_code = None
+            self.del_dt     = None
+        elif isinstance(row, dict):
+            self.id         = row["id"]
+            self.username   = row["username"]
+            self.password   = row["password"]
+            self.name       = row["name"]
+            self.salt       = row["salt"]
+            self.avatar     = row["avatar"]
+            self.email      = row["email"]
+            self.email_code = row["email_code"]
+            self.del_dt     = row["del_dt"]
         else:
-            row = cursor.fetchone()
-            if not row : raise ValueError("Cursor has no data")
-            data = dict((k, v) for k, v in zip(cursor.column_names, row))
-            self.id         = data["id"]
-            self.username   = data["username"]
-            self.password   = data["password"]
-            self.name       = data["name"]
-            self.salt       = data["salt"]
-            self.avatar     = data["avatar"]
-            self.email      = data["email"]
-            self.email_code = data["email_code"]
+            raise ValueError("row type unsupported")
+
+    def __repr__(self):
+        return str(self.__dict__)
 
 
 class UserDAO:
@@ -35,31 +40,107 @@ class UserDAO:
     def make_salt(self, length = 20):
         return random.randbytes(length).hex()
     
-    def hash_password(self, password: str, salt: str):
-        return hashlib.sha1((password + salt).encode()).hexdigest()
+    def hash_password(self, password, salt):
+        return hashlib \
+            .sha1((password + salt) \
+            .encode()) \
+            .hexdigest()
 
     def make_email_code(self):
         return self.make_salt(3)
 
     def create(self, user: User):
-        cursor = self.connection.cursor()
         user.id = str(uuid.uuid4())
         user.salt = self.make_salt()
         user.password = self.hash_password(user.password, user.salt)
         user.email_code = self.make_email_code()
         keys = user.__dict__.keys()
-        sel = ','.join(f"`{x}`" for x in keys)
+        columns = ','.join(f"`{x}`" for x in keys)
         values = ','.join(f"%({x})s" for x in keys)
-        sql = f"INSERT INTO users({sel}) VALUES({values})"
+        sql = f"INSERT INTO users({columns}) VALUES({values})"
         try:
+            cursor = self.connection.cursor()
             cursor.execute(sql, user.__dict__)
             self.connection.commit()
         except mysql.connector.Error as error:
-            print(f"Create: {error}")
+            print(f"Create failed: {error}")
         else:
             print("User created")
         finally:
             cursor.close()
+    
+    def read(self, id = None, username = None, ignore_deleted = True):
+        sql = "SELECT u.* FROM `users` AS u"
+        params = []
+        if id:
+            sql += " WHERE u.`id` = %s"
+            params.append(id)
+        if username:
+            sql += " AND" if id else " WHERE"
+            sql += " u.`username` = %s"
+            params.append(username)
+        if ignore_deleted:
+            sql += (" AND" if id or username else " WHERE") + \
+            " u.`del_dt` IS NULL"
+        try:
+            cursor = self.connection.cursor(dictionary = True)
+            cursor.execute(sql, params)
+        except mysql.connector.Error as error:
+            print(f"Read failed: {error}")
+        else:
+            return tuple(User(row) for row in cursor)
+        finally:
+            cursor.close()
+
+    def read_by_credentials(self, username, password):
+        user = (self.read(username = username) + (None,))[0]
+        if (user and self.hash_password(password, user.salt) == user.password):
+            return user
+        return None
+    
+    def update(self, user: User):
+        sql = "UPDATE `users` u SET "
+        data_to_edit = dict()
+        data_to_edit["id"] = user.id
+        for k, v in user.__dict__.items():
+            if v == None:
+                continue
+            data_to_edit[k] = v
+        sql += ",".join(f"u.`{k}` = %({k})s" for k in data_to_edit.keys())
+        sql += f" WHERE u.`id` = %(id)s"
+        try:
+            cursor = self.connection.cursor(dictionary = True)
+            cursor.execute(sql, data_to_edit)
+            self.connection.commit()
+        except mysql.connector.Error as error:
+            print(f"Update failed: {error}")
+        else:
+            print("User updated")
+        finally:
+            cursor.close()
+    
+    def delete(self, user: User):
+        sql = '''
+            UPDATE `users` AS u 
+            SET u.`del_dt` = %s 
+            WHERE u.`id` = %s
+        '''
+        try:
+            cursor = self.connection.cursor()
+            user.del_dt = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+            cursor.execute(sql, [user.del_dt, user.id])
+            self.connection.commit()
+        except mysql.connector.Error as error:
+            print(f"Delete failed: {error}")
+        else:
+            print("User deleted")
+            return True
+        finally:
+            try:
+                cursor.close()
+            except:
+                pass
+        return False
 
 
 def main(db_config):
@@ -71,14 +152,7 @@ def main(db_config):
     else:
         print("Connected")
     
-    user = User()
-    user.username = "david"
-    user.email = "david@gmail.com"
-    user.name = "David"
-    user.password = "123"
-
     user_dao = UserDAO(connection)
-    user_dao.create(user)
 
 
 if __name__ == "__main__":
