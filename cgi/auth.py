1f8bd7008c3207547e084bda2ac89fde38eb622f
Nikita Miller <milleriscream@gmail.com>
2022-12-19 13:02:46 +0200

    auth

--- cgi/auth.py
+++ cgi/auth.py
@@ -13,6 +13,13 @@ query = os.environ["QUERY_STRING"]
 
 content_length += len(query) + 1
 
+params = dict()
+for param in query.split('&'):
+    param = param.split('=')
+    params[param[0]] = param[1]
+
+content_length += len(str(params)) + 1
+
 headers = {}
 for k, v in os.environ.items():
     if k.startswith("HTTP_"):
@@ -36,5 +43,6 @@ print(f"Content-Length: {content_length}")
 print()
 print(method)
 print(query)
+print(params)
 print(headers)
 print(body)
